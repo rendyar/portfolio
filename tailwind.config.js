ec29ac5e1b1cb297add31424829131c221a7c6bb
rendyar <rendyafriatama11@gmail.com>
2022-06-24 16:15:40 +0700

    portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1d4ed8',
+        dark: '#0f172a',
+        secondary: '#475569',
+      },
+    },
+  },
+  plugins: [],
+}
